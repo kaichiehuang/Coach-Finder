71ce43171b70542a8b7d319ace66517e4b44a7e7
kaichiehuang <kaichiehuang@gmail.com>
2021-07-31 23:37:38 -0700

    Add sport filter

---
+++ src/components/coaches/coachFilter.vue
@@ -0,0 +1,77 @@
+<template>
+  <base-card>
+  <h2>Find Your Coach</h2>
+  <span class="filter-option">
+    <input type="checkbox" id="basketball" checked @change="setFilter"/>
+    <label for="basketball">Basketball</label>
+  </span>
+  <span class="filter-option">
+    <input type="checkbox" id="tennis" checked @change="setFilter"/>
+    <label for="tennis">Tennis</label>
+  </span>
+  <span class="filter-option">
+    <input type="checkbox" id="swimming" checked @change="setFilter"/>
+    <label for="swimming">Swimming</label>
+  </span>
+  <span class="filter-option">
+    <input type="checkbox" id="boxing" checked @change="setFilter"/>
+    <label for="boxing">Boxing</label>
+  </span>
+  <span class="filter-option">
+    <input type="checkbox" id="soccer" checked @change="setFilter"/>
+    <label for="soccer">Soccer</label>
+  </span>
+  </base-card>
+</template>
+
+<script> 
+export default {
+  emits: ['change-filter'],
+  data() {
+    return {
+      filters: {
+        basketball: true,
+        tennis: true,
+        swimming: true,
+        boxing: true,
+        soccer: true
+      }
+    };
+  },
+  methods: {
+    setFilter(event) {
+      const inputId = event.target.id;
+      const isActive = event.target.checked;
+      const updatedFilters = {
+        ...this.filters,
+        [inputId]: isActive
+      };
+      this.filters = updatedFilters;
+      this.$emit('change-filter', updatedFilters);
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align: middle;
+}
+
+.filter-option label {
+  margin-left: 0.25rem;
+}
+
+.filter-option.active label {
+  font-weight: bold;
+}
+</style>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,41 +1,74 @@
 <template>
-  <section>FILTER</section>
+  <section>
+    <coach-filter @change-filter="setFilters"></coach-filter>
+  </section>
   <section>
     <base-card>
-    <div class="controls">
-      <base-button mode="outline">Refresh</base-button>
-      <base-button link to="/register">Register as Coach</base-button>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :rate="coach.hourlyRate"
-        :sport-name="coach.sportName"
-        :levels="coach.levels"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :sport-name="coach.sportName"
+          :levels="coach.levels"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
     </base-card>
   </section>
 </template>
 
 <script>
-import CoachItem from '../../components/coaches/coachItem.vue';
-
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
 export default {
-  components: { CoachItem},
+  components: { CoachItem, CoachFilter },
+  data() {
+    return {
+      activeFilters: {
+        basketball: true,
+        tennis: true,
+        swimming: true,
+        boxing: true,
+        soccer: true
+      }
+    }
+  },
   computed: {
     filteredCoaches() {
-      return this.$store.getters['coaches/coaches'];
+      const coaches = this.$store.getters['coaches/coaches'];
+      return coaches.filter(coach => {
+        if (this.activeFilters.basketball && coach.sportName === 'basketball') {
+          return true;
+        } else if (this.activeFilters.tennis && coach.sportName === 'tennis') {
+          return true;
+        } else if (this.activeFilters.swimming && coach.sportName === 'swimming') {
+          return true;
+        } else if (this.activeFilters.boxing && coach.sportName === 'boxing') {
+          return true;
+        } else if (this.activeFilters.soccer && coach.sportName === 'soccer') {
+          return true;
+        } else {
+          return false;
+        }
+      });
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
     },
   },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
+    }
+  }
 };
 </script>
 
