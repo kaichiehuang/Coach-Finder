378777c66feffe3b644db704d98d717542d33d17
kaichiehuang <kaichiehuang@gmail.com>
2021-07-31 17:52:26 -0700

    Coach list and list items

---
+++ src/store/modules/coaches/index.js
@@ -0,0 +1,36 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'James',
+          lastName: 'Huang',
+          sportName: 'tennis',
+          levels: ['beginner', 'intermediate'],
+          description:
+            "I'm James and teach tennis!",
+          hourlyRate: 30
+        },
+        {
+          id: 'c2',
+          firstName: 'Julie',
+          lastName: 'Jones',
+          sportName: 'basketball',
+          levels: ['beginner', 'intermediate', 'pro'],
+          description:
+            'I am Julie and I am a former professional basketball player.',
+          hourlyRate: 40
+        }
+      ]
+    };
+  },
+  mutations,
+  actions,
+  getters
+};
