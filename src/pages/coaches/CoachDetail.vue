526c5fa27230da5b6b754914f9804b4580ac174d
kaichiehuang <kaichiehuang@gmail.com>
2021-08-01 10:21:50 -0700

    store requests to vuex

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -11,6 +11,7 @@
         <h2>Interested? Reach out now!</h2>
         <base-button link :to="contactLink">Contact</base-button>
       </header>
+      <router-view></router-view>
     </base-card>
   </section>
   <section>
